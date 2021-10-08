c234ec61228b98b2e3eaf4105c7d0fa6b33d1a6a
Nikola Vukovic <nikolav54@gmail.com>
2021-10-08 14:03:26 +0200

    Initial commit and project Node.js setup with MikroORM/Nodemon/Typescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants';
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [],
+        dbName: 'lireddit',
+        type: "postgresql",
+        debug: !__prod__,
+    });
+
+}
+
+
+main();
