47331d590b8d9e25b36e767a812dd3208c5ec78b
Nikola Vukovic <nikolav54@gmail.com>
2021-10-09 14:05:56 +0200

    Added and configured Mikro ORM and PostgreSQL DB

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities: [],
-        dbName: 'lireddit',
-        type: "postgresql",
-        debug: !__prod__,
-    });
+    //Connect to DB and run migrations
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
 
+    const post = orm.em.create(Post, { title: 'my first post'});
+    await orm.em.persistAndFlush(post);
 }
 
-
-main();
+main().catch(err => {
+    console.log(err);
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+require('dotenv').config();
+import path from 'path';
+
+export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d.[tj]s$/,
+    },
+    entities: [Post],
+    dbName: "lireddit",
+    user: "postgres",
+    password: "masina123",
+    type: "postgresql",
+    debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
