587b7c5778b1d6113db9ba84b2370eba3b51c8a0
Nikola Vukovic <nikolav54@gmail.com>
2021-10-09 14:39:08 +0200

    Added Apollo Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
     //Connect to DB and run migrations
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    const post = orm.em.create(Post, { title: 'my first post'});
-    await orm.em.persistAndFlush(post);
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        })
+    })
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({app});
+
+    app.listen(4040, () => {
+        console.log("App is listening on port: 4040")
+    });
 }
 
 main().catch(err => {
